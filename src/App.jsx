f236234482730a5457207520fe0dd95cd2d30ccb
Alison Souza <alison@contasimples.com>
2024-12-07 19:19:41 -0300

    feat: add Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/home/home";
 import Congratulations from "./pages/congrat/congrat";
 import Cards from "./pages/cards/cards";
 import Audio from "./pages/audio/audio";
+import { Analytics } from "@vercel/analytics/next";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Accept from "./pages/accept/accept";
@@ -19,6 +20,7 @@ function App() {
         <Route path="/eagora" element={<Accept />} />
         <Route path="/presente" element={<Gift />} />
         <Route path="/fim" element={<End />} />
+        <Analytics />
       </Routes>
     </Router>
   );
